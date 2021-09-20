f5072779928cbf09f22aca5b27a5c1bede413e74
Matheus <matheuso.silva95@gmail.com>
2021-09-20 12:37:24 -0300

    conexão com o DB

--- app.js
+++ app.js
@@ -2,6 +2,34 @@ const express = require("express");
 
 const app = express();
 
+//Conexão com Banco de Dados
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root@localhost',
+    password: 'Attera_6969',
+    database: 'node_project'
+});
+
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('connected as id ' + connection.threadId);
+  });
+
+connection.query('SELECT * FROM users', function(err, rows, fields){
+    if(!err){
+        console.log('Resultado', rows);
+    } else {
+        console.log('Erro ao realizar a consulta')
+    }
+})
+
+
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/src/index.html");
 });
