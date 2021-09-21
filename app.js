4d89c2ec0b1ce6e74afc5c90caa5d490ed2236da
Matheus <matheuso.silva95@gmail.com>
2021-09-21 06:46:33 -0300

    correcao conexão com DB

--- app.js
+++ app.js
@@ -1,43 +1,27 @@
-const express = require("express");
-
-const app = express();
-
-//Conexão com Banco de Dados
+//Conexao com BD MySQL
 const mysql = require('mysql');
 
+//A partir do MySQL 8 apresenta o erro ao utilizar o usuário root para conexão, necessário criar novo usuário (instrução no Readme)
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'matt_node@localhost',
+    host: 'localhost',
+    user: 'mattnode',
     password: 'Attera_6969',
-    database: 'node_project'
+    database: 'node_new'
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
+        console.error('error connecting: ' + err.stack);
+        return;
     }
-   
     console.log('connected as id ' + connection.threadId);
-  });
+});
 
 connection.query('SELECT * FROM users', function(err, rows, fields){
     if(!err){
-        console.log('Resultado', rows);
-    } else {
-        console.log('Erro ao realizar a consulta')
+        console.log('Resultado: ', rows);
+    }else{
+        console.log('Erro ao realizar a consulta');
     }
-})
-
-
-app.get("/", function(req,res){
-    res.sendFile(__dirname + "/src/index.html");
 });
-
-app.get("/contato", function(req,res){
-    res.send("Pagina de contato");
-});
-
-app.listen(8081);
-
  
